26c84fadf9556b854ee8ff584b4c679bf780ced6
Segy Hendro Pratama <segy.hendro@mekari.com>
2022-09-27 21:07:04 +0700

    init spring

---
+++ src/test/java/com/example/taskmaker/TaskMakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.taskmaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskMakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
