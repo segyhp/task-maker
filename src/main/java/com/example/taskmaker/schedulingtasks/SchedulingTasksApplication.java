8d6d75098997d297d7889b18b151901086161029
Segy Hendro Pratama <segy.hendro@mekari.com>
2022-09-28 16:57:22 +0700

    scheduling tasks

---
+++ src/main/java/com/example/taskmaker/schedulingtasks/SchedulingTasksApplication.java
@@ -0,0 +1,23 @@
+package com.example.taskmaker.schedulingtasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+//@SpringBootApplication
+//@EnableScheduling
+//public class SchedulingTasksApplication {
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(ScheduledTasks.class, args);
+//    }
+//}
+
+@SpringBootApplication
+@EnableScheduling
+public class SchedulingTasksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchedulingTasksApplication.class);
+    }
+}
